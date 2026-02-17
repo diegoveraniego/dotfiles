f17cbbe129c54c489cffa4151b0133608ff17edc
Diego <diegoaliaga2102@gmail.com>
2026-02-16 22:04:04 -0300

    fix: comprehensive org-block and org-quote styling. fixes #2

--- config.el
+++ config.el
@@ -1,11 +1,11 @@
-;;; config.el --- Configuración de diegoveraniego ---
+;; config.el --- Configuración de diegoveraniego ---
 
 ;; --- 1. IDENTIFICACIÓN y UI ---
 (setq user-full-name "Diego"
       display-line-numbers-type 'relative)
 
 (setq doom-theme 'doom-material)
-(setq fancy-splash-image (expand-file-name "splash_imgs/emacs.png" doom-private-dir))
+(setq fancy-splash-image (expand-file-name "splash_imgs/emacs_dark.png" doom-private-dir))
 
 ;; --- 2. FUENTES ---
 (setq doom-font (font-spec :family "JetBrains Mono" :size 14)
@@ -17,14 +17,26 @@
         org-hide-emphasis-markers t)
 
   (add-hook 'org-mode-hook #'org-modern-mode)
-  (add-hook 'org-mode-hook #'org-appear-mode)
+  (add-hook 'org-mode-hook #'org-appear-mode))
 
+;; Función para limpiar el fondo de bloques y citas (Soluciona Issue #2 definitivamente)
+(defun diego/apply-org-styles ()
+  "Asegura que bloques, citas y niveles se vean correctamente en cualquier tema."
   (custom-set-faces
+   ;; Bloques de código (Python/WebDev)
    '(org-block ((t (:inherit default :background nil :extend t))))
    '(org-block-begin-line ((t (:inherit org-meta-line :slant italic :background nil))))
    '(org-block-end-line ((t (:inherit org-block-begin-line :background nil))))
+
+   ;; Bloques de cita (Investigación/Tesis)
+   '(org-quote ((t (:inherit default :background nil :slant italic :extend t))))
+
+   ;; Niveles de encabezado (H1, H2)
    '(org-level-1 ((t (:foreground "#ff69b4" :height 1.3 :weight bold))))
    '(org-level-2 ((t (:height 1.1 :weight semi-bold))))))
+
+;; Hook para aplicar estilos al cargar cualquier tema
+(add-hook 'doom-load-theme-hook #'diego/apply-org-styles)
 
 ;; --- 4. PAQUETES EXTRAS (Nyan Cat) ---
 (use-package! nyan-mode
@@ -35,7 +47,12 @@
 ;; --- 5. MODO ESCRITURA (Zen) ---
 (after! writeroom
   (setq writeroom-width 90
-        writeroom-center-fixed-width t))
+        writeroom-center-fixed-width t)
+  ;; Fix Issue #1: Ocultar números de línea con timer
+  (add-hook 'writeroom-mode-hook
+            (lambda ()
+              (run-with-timer 0.1 nil
+                             (lambda () (display-line-numbers-mode -1))))))
 
 ;; --- 6. TOGGLE THEME (Light/Dark) ---
 (defun diego/toggle-theme ()
@@ -51,9 +68,13 @@
 
   (load-theme doom-theme t)
   (doom/reload-theme)
+
+  ;; Actualizamos la variable interna del banner para asegurar el cambio
+  (setq +doom-dashboard-banner-file fancy-splash-image)
+
   (when (get-buffer "+doom-dashboard")
     (with-current-buffer "+doom-dashboard"
-      (+doom-dashboard-reload))))
+      (+doom-dashboard-reload t))))
 
 (map! :leader
       :desc "Toggle dark/light theme"
